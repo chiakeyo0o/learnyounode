705de8e3f7588b2954325ed1413486c6f424a16f
chiakey <chiakeyo0o@gmail.com>
2016-07-06 15:23:14 +0800

    add p13

--- p13.js
+++ p13.js
@@ -1,1 +1,21 @@
-p13.js
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+  var parsed = url.parse(req.url, true);
+  var date = new Date(parsed.query.iso);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  if (parsed.pathname === '/api/parsetime') {
+    res.write(JSON.stringify({
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    }));
+  }
+
+  if (parsed.pathname === '/api/unixtime') {
+    res.write(JSON.stringify({ unixtime: Math.round(date.getTime()) }));
+  }
+  res.end()
+});
+server.listen(process.argv[2]);
